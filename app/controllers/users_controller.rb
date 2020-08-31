fd84cc005e982920e803a9f279c6ff4039841a92
mizugorou2019 <mizugorou2019@users.noreply.github.com>
2020-08-31 20:46:54 +0900

    ユーザー管理・修正3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,2 @@
 class UsersController < ApplicationController
-  def edit
-  end
-
-  def update
-  end
-
-  # def destroy
-  #   # user = User.find(params[:id])
-  #   # user.destroy
-  # end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end
 end
