d86178afc22039d6bea67474f7c28a94d8321df2
mizugorou2019 <mizugorou2019@users.noreply.github.com>
2020-08-31 18:08:47 +0900

    gem rails_12factorの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    # if current_user.update(user_params)
-    #   redirect_to root_path
-    # else
-    #   render :edit
-    # end
   end
-
-  # deviseの内部コントローラーがやってくれるので不要
-  # def new
-  # end
-
-  # def create
-  # end
 
   # def destroy
   #   # user = User.find(params[:id])
